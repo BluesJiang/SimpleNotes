e0b5f31d16303028ba5daa26d3230ac533f0371c
BluesJiang <Blues_J@Blues-MacBook-Pro.local>
2015-10-24 10:55:54 +0800

    Initial Commit

---
+++ SimpleNotes/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  SimpleNotes
+//
+//  Created by BluesJiang on 24/10/15.
+//  Copyright (c) 2015年 BluesJiang. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ SimpleNotes/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SimpleNotes
+//
+//  Created by BluesJiang on 24/10/15.
+//  Copyright (c) 2015年 BluesJiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
